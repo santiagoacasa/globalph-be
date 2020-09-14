9a112b23134f5b4fe9bfde179b4e52eca5b49b16
Santiago Casa <santiagoacasa@gmail.com>
2020-09-15 00:48:58 +0200

    Deploy a Heroku

---
+++ public/build/precache-manifest.39d4e950e53aa165e8d798d70d4974e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207028e1171f51f8b0b2367f1aa0e69b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeacfce7d6d33b5d81b1",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d002271701d0c6e2b505",
+    "url": "/static/css/main.6f135252.chunk.css"
+  },
+  {
+    "revision": "aeacfce7d6d33b5d81b1",
+    "url": "/static/js/2.8b3e7465.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8b3e7465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d002271701d0c6e2b505",
+    "url": "/static/js/main.445c7bda.chunk.js"
+  },
+  {
+    "revision": "fa91b60e096552867979",
+    "url": "/static/js/runtime-main.f4181f6f.js"
+  },
+  {
+    "revision": "0502a70691a631844d87fd3018741571",
+    "url": "/static/media/homepage-image.0502a706.jpg"
+  },
+  {
+    "revision": "aa9886477bf14090a770c92a1daf9ea1",
+    "url": "/static/media/homepage-image2.aa988647.jpg"
+  }
+]);
