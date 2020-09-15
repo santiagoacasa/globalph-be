892aaae8e3b54e1a2514201e5cf160a434993ca4
Santiago Casa <santiagoacasa@gmail.com>
2020-09-15 03:26:02 +0200

    Fixed update Consumer

--- routes/crud.routes.js
+++ routes/crud.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crudRoutes = express.Router();
 const Photographer = require('../models/Photographer.model');
+const Consumer = require('../models/Consumer.model')
 const uploadUserPic = require('../configs/cloudinaryProfilePics.config')
 
 
@@ -41,17 +42,32 @@ crudRoutes.post('/photographers', (req, res, next) => {
 crudRoutes.patch('/updateProfile', async (req, res, next) => {
   console.log(req.body)
   const {_id: userId } = req.session.currentUser
-  try {
-    updatedUser = await Photographer.findByIdAndUpdate({_id: userId}, req.body, {new: true})
-    req.session.currentUser = updatedUser
-    res.status(200).json({
-      message: "User successfully updated",
-      updatedUser: updatedUser})
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({
-      message: "Something went wrong updating the user, please try again later."})
+  if(req.session.currentUser.isPhotographer) {
+    try {
+      updatedUser = await Photographer.findByIdAndUpdate({_id: userId}, req.body, {new: true})
+      req.session.currentUser = updatedUser
+      res.status(200).json({
+        message: "User successfully updated",
+        updatedUser: updatedUser})
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({
+        message: "Something went wrong updating the user, please try again later."})
+    }
+  } else {
+    try {
+      updatedUser = await Consumer.findByIdAndUpdate({_id: userId}, req.body, {new: true})
+      req.session.currentUser = updatedUser
+      res.status(200).json({
+        message: "User successfully updated",
+        updatedUser: updatedUser})
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({
+        message: "Something went wrong updating the user, please try again later."})
+    }
   }
+  
 })
 
 module.exports = crudRoutes
