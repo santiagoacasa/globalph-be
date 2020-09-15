fa6673e135874a70cb625a707eb4ae762a35eca9
Santiago Casa <santiagoacasa@gmail.com>
2020-09-15 02:44:56 +0200

    test routes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(require('node-sass-middleware')({
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', (req, res) => {
+app.get('/***', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
